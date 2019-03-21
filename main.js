03cacf206b990bf542e1823404b8abff4572dd71
Sasquire <32607148+Sasquire@users.noreply.github.com>
2019-03-21 04:08:13 -0400

    added daily and large updates

--- main.js
+++ main.js
@@ -27,23 +27,30 @@ async function destroy(){
 		.catch(console.log);
 }
 
-/* async function add_post_to_db(post){
+async function add_post_to_db(post_id){
+	// todo handle when post 404's
+	const data = await e621.post_show_id(post_id);
+	const as_db_obj = [convert_to_post_obj(data)];
+	await db.query(add_posts_to_db_query[0], [JSON.stringify(as_db_obj)]);
+	if(data.status != 'deleted'){
+		const as_md5_obj = [convert_to_post_md5_obj(data)];
+		await db.query(add_posts_to_db_query[1], [JSON.stringify(as_md5_obj)]);
+	}
+}
 
-} */
-
+// splitting it on \n\n isnt that great of an idea
+// but pg can't insert multiple commands into a prepared statement 
+// so this was the second best thing I could think of
+// I'm not a fan of the global variable, but I don't
+// wan't to read the file every time.
 const add_posts_to_db_query = fs.readFileSync(dir+'/sql/insert_posts.sql', 'utf8').split('\n\n');
 async function add_posts_to_db(post_array){
-	const good_posts = JSON.stringify(post_array.map(convert_to_post_obj));
-	const good_md5s  = JSON.stringify(post_array
-		.filter(e => e.md5)
+	const good_posts = post_array.map(convert_to_post_obj);
+	const good_md5s  = post_array.filter(e => e.status != 'deleted')
 		.map(convert_to_post_md5_obj)
-	);
-	return db.query(add_posts_to_db_query[0], [good_posts])
-		.then(() => db.query(add_posts_to_db_query[1], [good_md5s]))
-		.catch(e => {
-			console.log(e, ' error');
-			throw e;
-		});
+	await db.query(add_posts_to_db_query[0], [JSON.stringify(good_posts)])
+	await db.query(add_posts_to_db_query[1], [JSON.stringify(good_md5s)]);
+	return;
 }
 
 function convert_to_post_md5_obj(raw_e621){
@@ -83,9 +90,55 @@ function convert_to_post_obj(raw_e621){
 	}
 }
 
-destroy()
-	.then(e => init())
-	.then(e => e621.$before_id_dense(1000000))
-	.then(e => add_posts_to_db(e))
-	.then(e => init())
-	.catch(e => console.log(e));
+async function daily_post_adding(){
+	const response = await db.query('select max(change_id) from posts;');
+	const max_change = response.rows[0].max;
+	console.log(`Max known change_id is ${max_change}`);
+
+	let page_num = 1;
+	while(true){
+		console.log(`Downloading page ${page_num}`);
+		const posts = await e621.post_list({
+			tags: `change:>${max_change} order:change`,
+			page: page_num
+		});
+		await add_posts_to_db(posts);
+
+		if(posts.length <= 320){ break; }
+		page_num++;
+	}
+
+	// todo check deleted_index / flagged_index
+}
+
+async function large_post_adding(){
+	let lowest_id_known = 50; // 1 billion
+	while(true){ // will force break;
+		console.log(`Checking posts below id:${lowest_id_known}`);
+		
+		const raw_posts = await e621.$before_id_dense(lowest_id_known);
+		const posts = raw_posts.filter(p => p.status != 'destroyed');
+		if(posts.length == 0){ break; }
+		
+		await add_posts_to_db(posts);
+		lowest_id_known = posts.sort((a, b) => b.id - a.id).slice(-1)[0].id;
+	}
+}
+
+async function parse_args(){
+	await init();
+
+	const args = process.argv.slice(2).map(e => e.toLowerCase());
+	switch(args[0]){
+		case 'u':
+		case 'a':
+		case 'update':
+		case 'add': (() => { switch(args[1]){
+			case 'daily': return daily_post_adding();
+			case 'large': return large_post_adding();
+			case 'post' : return add_post_to_db(parseInt(args[2], 10));	
+		}})(); break;
+	}
+}
+
+parse_args();
--- sql/insert_posts.sql
+++ sql/insert_posts.sql
@@ -1,7 +1,21 @@
 insert into posts 
 select * from json_populate_recordset(null::posts, $1::json)
-on conflict (post_id) do nothing;
+on conflict (post_id) do update set
+	change_id = EXCLUDED.change_id,
+	created_at = EXCLUDED.created_at,
+	status = EXCLUDED.status,
+	flag_message = EXCLUDED.flag_message,
+	rating = EXCLUDED.rating,
+	tags = EXCLUDED.tags,
+	locked_tags = EXCLUDED.locked_tags,
+	sources = EXCLUDED.sources,
+	fav_count = EXCLUDED.fav_count,
+	score = EXCLUDED.score,
+	description = EXCLUDED.description,
+	parent_id = EXCLUDED.parent_id;
+-- creator_id, file_size, width, and height never change
 
 insert into post_md5s
 select * from json_populate_recordset(null::post_md5s, $1::json)
 on conflict (post_id) do nothing;
+-- should never replace md5s 
